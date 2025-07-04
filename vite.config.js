3d9b150868a831202767e25699c958403f58468d
occhi0 <35575152+occhi0@users.noreply.github.com>
2025-07-04 19:41:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Imposta il percorso corretto per GitHub Pages
+  base: '/fairytale-generators/', 
   plugins: [react()],
 })
